655edba7414b1d2640da978adedc59fc2e272bfb
Elske Sne <domschuk@yahoo.com>
2024-10-06 17:52:26 -0400

    removed erros

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -2,12 +2,6 @@
 var globalVar = "I'm a global variable";
 let globalLet = "I'm also global, but scoped with let";
 const globalConst = "I'm a global constant";
-{
-// Block scope
-var blockVar = "I'm a block-scoped var";
-let blockLet = "I'm a block-scoped let";
-const blockConst = "I'm a block-scoped const";
-}
 
 
 // Global scope
@@ -15,21 +9,6 @@ console.log(globalVar); // Output: "I'm a global variable"
 console.log(globalLet); // Output: "I'm also global, but scoped with let"
 console.log(globalConst); // Output: "I'm a global constant"
 
-//Block Scope
-//console.log(blockVar);
-//console.log(blockLet);
-
-function show(){
-var functionVar = "I'm a block-scoped var";
-let functionLet = "I'm a block-scoped let";
-const functionConst = "I'm a block-scoped const";
-console.log(functionVar);
-console.log(functionLet);
-console.log(functionConst);
-var functionVar = "I'm re-assigned var";
-console.log(functionVar);
-}
-show();
 
 
 
